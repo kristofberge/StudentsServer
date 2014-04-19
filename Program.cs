7703b5fb59af981c735f96cffd39b9e13fdbbd13
Kristof Bergé <kristof.berge@gmail.com>
2014-04-19 09:49:02 -0300

    Command based testprogram.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace StudentsServer
+{
+    class Program
+    {
+        static Server server;
+        static Thread serverThread;
+        
+        static void Main(string[] args)
+        {
+            server = new Server("192.168.0.105", new EchoResponseHelper());
+            serverThread = new Thread(new ThreadStart(() => server.startListening(666)));
+            serverThread.Start();
+            Client.start();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudentsServer
+{
+    class Server
+    {
+
+        private IPAddress _ipAddress;
+        private AbstractResponseHelper _helper;
+
+        private bool _continue;
+
+        public Server(string ipAddress, AbstractResponseHelper helper)
+        {
+            try
+            {
+                _ipAddress = IPAddress.Parse(ipAddress);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            this._helper = helper;
+        }
+
+        public void startListening(int port) {
+            try
+            {
+                _continue = true;
+                while (_continue)
+                {
+                    Thread.Sleep(10);
+                    TcpListener listener = new TcpListener(_ipAddress, port);
+
+                    listener.Start();
+
+                    Console.WriteLine("Listening on port " + port);
+                    Console.WriteLine("The local End point is: " + listener.LocalEndpoint);
+
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Connection accepted from " + client.ToString());
+
+                    byte[] bytes = new byte[256];
+
+                    NetworkStream stream = client.GetStream();
+                    stream.Read(bytes, 0, bytes.Length);
+                    _helper.Respond(client, stream, bytes);
+
+                    stream.Close();
+                    client.Close();
+                    listener.Stop();
+                }
+                Console.WriteLine("server stopped");
+                
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        public void RequestStop() 
+        {
+            Console.WriteLine("stop requested: " + _ipAddress.ToString());
+            _continue = false;
+        }
+    }
+}
